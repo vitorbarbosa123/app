d3fbffe2ee625ef277ecdfe6eb4281239213fd48
vitor barbosa <vitorbarbosa789@gmail.com>
2021-10-26 15:26:44 -0300

    componente MessageList + styles

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,20 +1,52 @@
-import React from 'react'
-import { ScrollView } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { ScrollView } from 'react-native';
+import { api } from '../../services/api';
+import { io } from 'socket.io-client';
 
-import { Message } from '../Message'
+import { MESSAGES_EXAMPLE } from '../../../assets/utils/messages';
 
-import { styles } from './styles'
+import { Message, MessageProps } from '../Message';
 
-export function MessageList(){
-    return (
-        <ScrollView 
-            style={ styles.container}
-            contentContainerStyle={styles.content}
-            keyboardShouldPersistTaps="never"
-        >
-           <Message/>
-           <Message/>
-           <Message/>
-        </ScrollView>
-    )
+import { styles } from './styles';
+
+let messagesQueue: MessageProps[] = MESSAGES_EXAMPLE;
+
+const socket = io(String(api.defaults.baseURL));
+socket.on('new_message', (newMessage) => {
+  messagesQueue.push(newMessage);
+  console.log(newMessage);
+});
+
+export function MessageList() {
+  const [currentMessages, setCurrentMessages] = useState<MessageProps[]>([]);
+
+  useEffect(() => {
+    async function fetchMessages() {
+      const messagesResponse = await api.get<MessageProps[]>('/messages/last3');
+      setCurrentMessages(messagesResponse.data);
+    }
+
+    fetchMessages();
+  }, [])
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setCurrentMessages(prevState => [messagesQueue[0], prevState[0], prevState[1]]);
+        messagesQueue.shift();
+      }
+    }, 3000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.content}
+      keyboardShouldPersistTaps="never"
+    >
+      {currentMessages.map((message) => <Message key={message.id} data={message} />)}
+    </ScrollView>
+  );
 }
--- src/components/MessageList/styles.ts
+++ src/components/MessageList/styles.ts
@@ -1,12 +1,12 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingHorizontal: 20
-    },
-    content: {
-        paddingTop: 135,
-        paddingBottom: 184
-    }
-})
+  container: {
+    flex: 1,
+    paddingHorizontal: 20
+  },
+  content: {
+    paddingTop: 135,
+    paddingBottom: 184
+  }
+});
