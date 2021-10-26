06fa1dd036821844da86993814e79af629243d55
vitor barbosa <vitorbarbosa789@gmail.com>
2021-10-26 15:26:24 -0300

    componente message + styles

--- src/components/Message/styles.ts
+++ src/components/Message/styles.ts
@@ -1,27 +1,27 @@
-import { StyleSheet } from "react-native";
-import { colors, fonts } from "../../../assets/theme";
+import { StyleSheet } from 'react-native';
+import { COLORS, FONTS } from '../../theme';
 
 export const styles = StyleSheet.create({
-    container: {
-        width: '100%',
-        marginBottom: 36
-    },
-    message: {
-        fontSize: 15,
-        fontFamily: fonts.REGULAR,
-        color: colors.WHITE,
-        lineHeight: 20,
-        marginBottom: 12
-    },
-    userName: {
-        fontSize: 15,
-        fontFamily: fonts.REGULAR,
-        color: colors.WHITE,
-        marginLeft: 16
-    },
-    footer: {
-        width: '100%',
-        flexDirection: 'row',
-        alignItems: 'center'
-    }
-})
+  container: {
+    width: '100%',
+    marginBottom: 36,
+  },
+  message: {
+    fontSize: 15,
+    fontFamily: FONTS.REGULAR,
+    color: COLORS.WHITE,
+    lineHeight: 20,
+    marginBottom: 12
+  },
+  footer: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  userName: {
+    fontSize: 15,
+    fontFamily: FONTS.REGULAR,
+    color: COLORS.WHITE,
+    marginLeft: 16
+  }
+});
