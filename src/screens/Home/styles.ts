33ed56eba8989060b2ee4fbe8229aba7a23ab194
vitor barbosa <vitorbarbosa789@gmail.com>
2021-10-26 15:27:27 -0300

    melhorias na home

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,11 +1,11 @@
 import { StyleSheet } from 'react-native';
-import { colors } from '../../../assets/theme';
+import { COLORS } from '../../theme';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: colors.BLACK_SECONDARY,
-        paddingTop: getStatusBarHeight() + 17
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.BLACK_SECONDARY,
+    paddingTop: getStatusBarHeight() + 17
+  }
+});
