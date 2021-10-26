aea894c3ddc1bea42b4f0ed322e0c06056aad7f8
vitor barbosa <vitorbarbosa789@gmail.com>
2021-10-26 15:25:50 -0300

    correção no button + styles

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,17 +1,19 @@
-import { StyleSheet } from "react-native";
-import { fonts } from "../../../assets/theme";
+import { StyleSheet } from 'react-native';
+import { FONTS } from '../../../assets/theme';
 
 export const styles = StyleSheet.create({
-    button: {
-       height: 48,
-       width: '100%',
-       flexDirection: 'row',
-       alignItems: 'center',
-       justifyContent: 'center'
-    },
-    title: {
-        fontSize: 14,
-        fontFamily: fonts.BOLD
-    }
-
-})
+  button: {
+    height: 48,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontSize: 14,
+    fontFamily: FONTS.BOLD
+  },
+  icon: {
+    marginRight: 12
+  }
+});
